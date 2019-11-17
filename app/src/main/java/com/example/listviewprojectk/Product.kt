8554b9532da471754b4eb9d4f51b1d9d7f98adc5
IvanovAdrew <zyablik7771@gmail.com>
2019-11-17 15:50:55 +0200

    now really

--- app/src/main/java/com/example/listviewprojectk/Product.kt
+++ app/src/main/java/com/example/listviewprojectk/Product.kt
@@ -1,4 +1,31 @@
 package com.example.listviewprojectk
 
+import androidx.annotation.DrawableRes
+
 class Product {
+    private var name: String
+        get() {return this.name}
+        set(value) {this.name=value}
+    private var costOfProducts: Int
+        get() {
+            return (costOfProducts)
+        }
+        set(value) {this.costOfProducts=value}
+    @get:DrawableRes
+    private var photoId: Int
+    get() {return photoId}
+    set(value) {this.photoId=value}
+    private var quantity: Int
+        get()= quantity
+        set(value){this.quantity=value}
+    private var costOfAllInEach: Int
+        get()= costOfAllInEach
+        set(value){this.costOfAllInEach=value}
+        companion object{
+            private var costOfaLL: Int
+            get()= costOfaLL
+            set(value){this.costOfaLL=value}
+        }
+
+
 }
