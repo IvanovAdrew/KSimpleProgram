ac54c9128133fe66dfb3d8cfbfe039c114aca6d5
IvanovAdrew <zyablik7771@gmail.com>
2019-11-17 16:07:39 +0200

    Product finnaly and NeArratAdapter

--- app/src/main/java/com/example/listviewprojectk/Product.kt
+++ app/src/main/java/com/example/listviewprojectk/Product.kt
@@ -2,30 +2,18 @@ package com.example.listviewprojectk
 
 import androidx.annotation.DrawableRes
 
-class Product {
-    private var name: String
-        get() {return this.name}
-        set(value) {this.name=value}
-    private var costOfProducts: Int
-        get() {
-            return (costOfProducts)
-        }
-        set(value) {this.costOfProducts=value}
-    @get:DrawableRes
-    private var photoId: Int
-    get() {return photoId}
-    set(value) {this.photoId=value}
+class Product(var name: String,var costOfProducts: Int,@get:DrawableRes var photoId: Int) {
     private var quantity: Int
         get()= quantity
         set(value){this.quantity=value}
     private var costOfAllInEach: Int
         get()= costOfAllInEach
         set(value){this.costOfAllInEach=value}
-        companion object{
-            private var costOfaLL: Int
+    companion object{
+        private var costOfaLL: Int
             get()= costOfaLL
             set(value){this.costOfaLL=value}
-        }
+    }
 
 
 }
