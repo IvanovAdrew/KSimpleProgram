da3d19f0bdb307e51add170e4ea6ee3ac762ad66
IvanovAdrew <zyablik7771@gmail.com>
2019-11-18 22:19:54 +0200

    work with NeArrayAdapter part 1

--- app/src/main/java/com/example/listviewprojectk/NeArrayAdapter.kt
+++ app/src/main/java/com/example/listviewprojectk/NeArrayAdapter.kt
@@ -1,2 +1,28 @@
 package com.example.listviewprojectk
 
+import android.content.Context;
+import androidx.annotation.DrawableRes;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class NeArrayAdapter : ArrayAdapter<Product> {
+
+    private lateinit var listener: ProductAmountChangesListener
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        return super.getView(position, convertView, parent)
+    }
+    constructor(context: Context,resource: Int,products: Array<Product>) : super(context, resource, products)
+    fun srtListener(listener: ProductAmountChangesListener){
+        this.listener=listener
+    }
+
+    interface ProductAmountChangesListener {
+        fun onChanged()
+    }
+}
